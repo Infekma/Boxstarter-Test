3600c25e4ce103c67e16bf77fcc5b63a3a2b5657
Alexander Dos Santos <de_atd@hotmail.com>
2020-10-04 01:02:02 +0100

    Update Setup-Environment.ps1

--- Setup-Environment.ps1
+++ Setup-Environment.ps1
@@ -1,1 +1,14 @@
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Latest.txt -DisableReboots
+
+# START - setup boost
+
+# download the boost setup python script
+$fileUrl = 'https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Test/setup-boost.py'
+$output = "$PSScriptRoot\setup-boost.py"
+(New-Object System.Net.WebClient).DownloadFile($fileUrl, $output)
+
+# run the boost setup script and then delete it afterwards
+python $output
+Remove-Item($output)
+
+# END - setup boost
