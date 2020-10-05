3610620c60fe1f46dd851ca6a43ab4bb9a3a6829
Infekma <de_atd@hotmail.com>
2020-10-05 23:25:08 +0100

    added main entry point for building boost
    added main entry point script for setting up boost, downloads all the required python scripts from the git repo and executes the correct order
    changed Setup-Environment.ps1 entry point to that new mian entry point for setting up boost.

---
+++ Boost/main.py
@@ -0,0 +1,36 @@
+from urllib.request import urlopen
+import tempfile
+import ntpath
+import os
+
+# file dependencies, these are other python scripts that need to be downloaded from the github
+file_dependencies = [
+    r"https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Boost/download_and_extract_boost.py",
+    r"https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Boost/download_from_web.py",
+    r"https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Boost/main_setup_boost.py",
+    ]
+
+def download_python_file(url, output_dir):
+    print(f"downloading python script from {url}")
+    file_name = ntpath.basename(url)
+    text_file = open(f"{output_dir}/{file_name}", "w",encoding='utf-8')
+    text_file.write(urlopen(url).read().decode('utf-8'))
+    text_file.close()
+
+with tempfile.TemporaryDirectory() as temp_dir:
+    print(f"downloading python scripts to temp dir: {temp_dir}")
+
+    # iterate through each script and download
+    for pyscript_url in file_dependencies:
+        download_python_file(pyscript_url, temp_dir)
+    
+    print("Finished downloading all python scripts")
+    print("Start executing scripts to setup boost")
+
+    # now that all the scripts are downloaded, execute the files
+    # 1. downloads boost and installs to C:/boost_build
+    # 2. sets up boost by running bootstrap and b2 and installs C:/boost
+    exec(open(f"{temp_dir}/download_and_extract_boost.py").read())
+    exec(open(f"{temp_dir}/main_setup_boost.py".read()))
+
+    print("Finished executing scripts")
--- Setup-Environment.ps1
+++ Setup-Environment.ps1
@@ -1,8 +1,8 @@
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Latest.txt -DisableReboots
 
 # download the boost setup python script
-$fileUrl = 'https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Boost/main_setup_boost.py'
-$output = "$PSScriptRoot\main_setup_boost.py"
+$fileUrl = 'https://raw.githubusercontent.com/Infekma/Boxstarter-Test/main/Boost/main.py'
+$output = "$PSScriptRoot\main.py"
 (New-Object System.Net.WebClient).DownloadFile($fileUrl, $output)
 
 # run the boost setup script and then delete it afterwards
