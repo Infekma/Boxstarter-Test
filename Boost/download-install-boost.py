32e8627a01a9a11fc78486cbfed95a38e643056b
Infekma <de_atd@hotmail.com>
2020-10-05 22:41:30 +0100

    further updates to automating boost
    renamed "Setup-Boost.py" to "main_setup-boost.py" and corresponding references
    added download_from_web.py which manages downloads from the web
    added download_and_extract_boost which downloads and extracts to C:/boost_build/
    added main_setup_boost.py which uses the C:/Boost_build/ and runs the bootstrap/b2
    removed boost\build_boost.bat

--- Boost/download-install-boost.py
+++
@@ -1,4 +0,0 @@
-
-boost_installer_exe_url_32 = r"https://downloads.sourceforge.net/project/boost/boost-binaries/1.67.0/boost_1_67_0-msvc-14.1-32.exe"
-boost_installer_exe_url_64 = r"https://downloads.sourceforge.net/project/boost/boost-binaries/1.67.0/boost_1_67_0-msvc-14.1-64.exe"
-
---
+++ Boost/download_and_extract_boost.py
@@ -0,0 +1,26 @@
+from download_from_web import download_file
+import zipfile
+import os
+import tempfile
+
+# the code currently assumes we're always downloading a zip
+boost_download_url = r"https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.zip"
+boost_output_dir = r"C:\boost_build"
+
+# create temporary directory to work at
+with tempfile.TemporaryDirectory() as temp_download_dir:
+    print(f'created temporary directory at {temp_download_dir}')
+
+    file_output_dir = f"{temp_download_dir}/boost.zip"
+    # download the boost zip file
+    download_file(boost_download_url, file_output_dir)
+
+    # TODO: add progress bar for zip progress?
+    # extract the downloaded file
+    with zipfile.ZipFile(file_output_dir, 'r') as zip_ref:
+        zip_ref.extractall(temp_download_dir)
+
+        # boost stores everything inside a directory, namely: boost_x_x_x..
+        # this step renames the folder and relocates it to "boost_output_dir"
+        output_folder = zip_ref.namelist()[0]
+        os.rename(f"{temp_download_dir}/{output_folder}", boost_output_dir)
