e8c6f9f0b021550aa097826efce433009cb28327
Infekma <de_atd@hotmail.com>
2020-10-06 20:13:17 +0100

    attempt at streamlining the entire process by automatically downloading the next script

--- Install-Boxstarter.ps1
+++ Install-Boxstarter.ps1
@@ -1,3 +1,12 @@
-Set-ExecutionPolicy Unrestricted
-iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
-Set-ExecutionPolicy Restricted
+# save and restore the execution policy after we finish
+$prevExecPolicy = Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted -Force
+$WebClient = New-Object System.Net.WebClient
+iex (WebClient.DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
+
+# start the box starter shell
+BOXSTARTERSHELL 
+iex (WebClient.DownloadString('https://github.com/Infekma/Boxstarter-Test/blob/main/Setup-Environment.ps1'));
+
+# recover the old execution policy
+Set-ExecutionPolicy $prevExecPolicy -Force
