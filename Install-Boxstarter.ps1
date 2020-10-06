3503c754c8c29461dc6ee5896332d25dd66abf44
Infekma <de_atd@hotmail.com>
2020-10-06 20:14:30 +0100

    forgot to add variable tags for WebClient

--- Install-Boxstarter.ps1
+++ Install-Boxstarter.ps1
@@ -2,11 +2,11 @@
 $prevExecPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy Unrestricted -Force
 $WebClient = New-Object System.Net.WebClient
-iex (WebClient.DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
+iex ($WebClient.DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
 
 # start the box starter shell
 BOXSTARTERSHELL 
-iex (WebClient.DownloadString('https://github.com/Infekma/Boxstarter-Test/blob/main/Setup-Environment.ps1'));
+iex ($WebClient.DownloadString('https://github.com/Infekma/Boxstarter-Test/blob/main/Setup-Environment.ps1'));
 
 # recover the old execution policy
 Set-ExecutionPolicy $prevExecPolicy -Force
