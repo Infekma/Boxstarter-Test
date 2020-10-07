9ebac86937aea37e5eb6124bc1a9726ef632f9ff
Infekma <de_atd@hotmail.com>
2020-10-07 22:11:27 +0100

    test custom package for installing installer tools (nsis + plugins)

---
+++ Custom Packages/installer-tools/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyUnzip 
+	-PackageName "nsis-shell-link"
+	-FileFillPuth "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\files\\Shelllink.zip"
+	-Destination $toolsdir
+
+Get-ChocolateyUnzip 
+	-PackageName "nsis-access-control"
+	-FileFillPuth "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\files\\AccessControl.zip.zip"
+	-Destination $toolsdir
